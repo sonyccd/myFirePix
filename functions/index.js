6ab9818db8122652b1c940bc6638cb586c55f58d
Bradford Bazemore <bankersalog@gmail.com>
2018-03-07 20:09:52 -0500

    init

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Triggers when a user gets a new follower and sends notifications if the user has enabled them.
+ * Also avoids sending multiple notifications for the same user by keeping a timestamp of sent notifications.
+ */
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'sendFollowerNotification') {
+  exports.sendFollowerNotification = require('./sendFollowerNotification').default;
+}
+
+/**
+ * When an image is uploaded we check if it is flagged as Adult or Violence by the Cloud Vision
+ * API and if it is we blur it using ImageMagick.
+ */
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'blurOffensiveImages') {
+  exports.blurOffensiveImages = require('./blurOffensiveImages').default;
+}
+
+/**
+ * When an account is deleted we delete all the user data in the store as well.
+ */
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'cleanupAccount') {
+  exports.cleanupAccount = require('./cleanupAccount').default;
+}
---
+++ public/scripts/auth.js
@@ -0,0 +1,92 @@
+'use strict';
+
+window.friendlyPix = window.friendlyPix || {};
+
+/**
+ * Handles the user auth flows and updating the UI depending on the auth state.
+ */
+friendlyPix.Auth = class {
+
+  /**
+   * Returns a Promise that completes when auth is ready.
+   * @return Promise
+   */
+  get waitForAuth() {
+    return this._waitForAuthPromiseResolver.promise();
+  }
+
+  /**
+   * Initializes Friendly Pix's auth.
+   * Binds the auth related UI components and handles the auth flow.
+   * @constructor
+   */
+  constructor() {
+    // Firebase SDK
+    this.database = firebase.database();
+    this.auth = firebase.auth();
+    this._waitForAuthPromiseResolver = new $.Deferred();
+
+    $(document).ready(() => {
+      // Pointers to DOM Elements
+      const signedInUserContainer = $('.fp-signed-in-user-container');
+      this.signedInUserAvatar = $('.fp-avatar', signedInUserContainer);
+      this.signedInUsername = $('.fp-username', signedInUserContainer);
+      this.signOutButton = $('.fp-sign-out');
+      this.deleteAccountButton = $('.fp-delete-account');
+      this.signedOutOnlyElements = $('.fp-signed-out-only');
+      this.signedInOnlyElements = $('.fp-signed-in-only');
+      this.usernameLink = $('.fp-usernamelink');
+
+      // Event bindings
+      this.signOutButton.click(() => this.auth.signOut());
+      this.deleteAccountButton.click(() => this.deleteAccount());
+      this.signedInOnlyElements.hide();
+    });
+
+    this.auth.onAuthStateChanged(user => this.onAuthStateChanged(user));
+  }
+
+  /**
+   * Displays the signed-in user information in the UI or hides it and displays the
+   * "Sign-In" button if the user isn't signed-in.
+   */
+  onAuthStateChanged(user) {
+    if (window.friendlyPix.router) {
+      window.friendlyPix.router.reloadPage();
+    }
+    this._waitForAuthPromiseResolver.resolve();
+    $(document).ready(() => {
+      if (!user) {
+        this.signedOutOnlyElements.show();
+        this.signedInOnlyElements.hide();
+        this.userId = null;
+        this.signedInUserAvatar.css('background-image', '');
+        firebaseUi.start('#firebaseui-auth-container', uiConfig);
+      } else {
+        this.signedOutOnlyElements.hide();
+        this.signedInOnlyElements.show();
+        this.userId = user.uid;
+        this.signedInUserAvatar.css('background-image',
+            `url("${user.photoURL || '/images/silhouette.jpg'}")`);
+        this.signedInUsername.text(user.displayName || 'Anonymous');
+        this.usernameLink.attr('href', `/user/${user.uid}`);
+        friendlyPix.firebase.saveUserData(user.photoURL, user.displayName);
+      }
+    });
+  }
+
+  deleteAccount() {
+    this.auth.currentUser.delete().then(() => {
+      window.alert('Account deleted');
+    }).catch(error => {
+      if (error.code === 'auth/requires-recent-login') {
+        window.alert(
+          'You need to have recently signed-in to delete your account.\n' +
+            'Please sign-in and try again.');
+        this.auth.signOut();
+      }
+    });
+  };
+};
+
+friendlyPix.auth = new friendlyPix.Auth();
